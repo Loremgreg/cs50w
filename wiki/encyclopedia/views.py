5b4d57a09504fec97c1bf4b0c95d5e489bc6caba
Loremgreg <gregorybastin.de@gmail.com>
2025-12-14 10:04:05 +0100

    Entry Page: Visiting /wiki/TITLE, where TITLE is the title of an encyclopedia entry, should render a page that displays the contents of that encyclopedia entry.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+# from django.http import HttpResponseRedirect
+from django.http import Http404
+import markdown2
+
 
 from . import util
 
@@ -8,8 +12,26 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def entry(request):
-    return render(request, "encyclopedia/title.html", {
-        "entries": util.get_entry()
-    })
+def entry(request, title):
+# La def doit :
+# 	1.	Recevoir le nom recherché
+# 	2.	Vérifier si le fichier existe
+# 	3.	Lire le contenu du fichier .md
+# 	4.	L’envoyer au template    
+    # On récupère le contenu de l'entrée en utilisant le titre fourni dans l'URL
+    content = util.get_entry(title)
+
+    # Cas 1 : L'entrée n'existe pas
+    if content is None:
+            raise Http404(f"{title} does not exist")
+    
+    # Cas 2 : L'entrée existe
+    else:
+        # On convertit le contenu Markdown en HTML
+        content = markdown2.markdown(content)
+        # On affiche la page de l'entrée avec son titre et son contenu
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": content,
+        })
 
